8638999337de724db15463a9a6e5fccbfeed589d
Rmolimock <495@holbertonschool.com>
2019-06-29 00:11:07 +0000

    deserialize from dictionary if passed to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,16 +2,22 @@
 '''
 '''
 from datetime import datetime
+import dateutil.parser
 from uuid import uuid4
 class BaseModel:
     '''
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         '''
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs == {}:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            self.id = kwargs['id']
+            self.created_at = dateutil.parser.parse(kwargs['created_at'])
+            self.updated_at = dateutil.parser.parse(kwargs['updated_at'])
     def __str__(self):
         '''
         '''
