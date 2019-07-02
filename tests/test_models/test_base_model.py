794ebbc3c60a42501b2c05fdbe6dc57e11e20d63
Rmolimock <495@holbertonschool.com>
2019-07-02 23:33:57 +0000

    Placeholder testBase class

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,35 @@
-#!/usr/bin/env pyhton3
+#!/usr/bin/python3
+"""
+    Unittest for Base class
+"""
+import unittest
+from models.base_model import BaseModel
+class Test_base_model_class(unittest.TestCase):
+    """ Unittest for Base class"""
+    b = BaseModel()
+    a = BaseModel()
+    c = BaseModel()
+    def test_unique_id(self):
+        """ test to confirm that the id's are unique"""
+        self.assertFalse(self.a.id is self.b.id)
+        self.assertFalse(self.a.id is self.c.id)
+        self.assertFalse(self.b.id is self.c.id)
+    def test_Base_Model_Creation(self):
+        """ Test to confirm instance of Base class"""
+        self.assertTrue(isinstance(self.b, BaseModel))
+        self.assertTrue(type(self.b) is BaseModel)
+    def test_id_is_string(self):
+        """ Test to make sure uuid.uuid4() was
+            converted to string
+        """
+        self.assertTrue(type(self.b.id) is str)
+    def test_to_dict(self):
+        """ test that to_dict produces a string"""
+        self.assertTrue(type(self.b.to_dict()) is dict)
+    def test_save(self):
+        """ test to make sure the time/date are
+            being changed
+        """
+        self.b.save()
+        self.assertNotEqual(self.b.created_at,
+                            self.b.updated_at)
