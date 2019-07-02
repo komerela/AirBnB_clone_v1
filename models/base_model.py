162ff562ed7d43567a3647e7d391e432ab436042
Rmolimock <495@holbertonschool.com>
2019-07-02 22:31:43 +0000

    pep8 style compliance

--- models/base_model.py
+++ models/base_model.py
@@ -19,9 +19,11 @@ class BaseModel:
                 if key == '__class__':
                     continue
                 elif key == "created_at":
-                    self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.created_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.updated_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == 'id':
                     self.id = value
                 else:
@@ -31,20 +33,25 @@ class BaseModel:
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
         models.storage.new(self)
+
     def save(self):
         models.storage.save()
+
     def __str__(self):
         '''
         '''
         return("[BaseModel] ({:s}) {:s}".format(self.id, str(self.__dict__)))
+
     def save(self):
         '''
         '''
         self.updated_at = datetime.utcnow()
-        list_of_dicts = [obj.to_dict() for (key, obj) in models.storage.all().items()]
+        list_of_dicts = [obj.to_dict() for (key, obj) in
+                         models.storage.all().items()]
         print(type(list_of_dicts))
         with open(models.storage.file_path(), "w+", encoding="utf-8") as f:
             f.write(json.dumps(list_of_dicts))
+
     def to_dict(self):
         '''
         '''
