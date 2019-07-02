70835570c00493cc22653f29ba41c2b7109b955b
komerela <kmwongera@icloud.com>
2019-07-02 19:12:49 +0000

    BaseMOdel class for all the methods for running the console

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,8 @@
 from datetime import datetime
 import dateutil.parser
 from uuid import uuid4
+import models
+import json
 
 
 class BaseModel:
@@ -16,25 +18,30 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
         else:
             self.id = kwargs['id']
             self.created_at = dateutil.parser.parse(kwargs['created_at'])
             self.updated_at = dateutil.parser.parse(kwargs['updated_at'])
-
+    def save(self):
+        models.storage.save()
     def __str__(self):
         '''
         '''
         return("[BaseModel] ({:s}) {:s}".format(self.id, str(self.__dict__)))
-
     def save(self):
         '''
         '''
+        objects_dict = {}
+        for (key, obj) in models.storage.all().items():
+            objects_dict[key] = obj.to_dict()
+        with open(models.storage.file_path, "w", encoding="utf-8") as f:
+            json.dump(objects_dict, f)
         self.updated_at = datetime.now()
-
     def to_dict(self):
         '''
         '''
-        mydict = self.__dict__
+        mydict = self.__dict__.copy()
         mydict["__class__"] = "BaseModel"
         mydict["updated_at"] = self.updated_at.isoformat()
         mydict["created_at"] = self.created_at.isoformat()
