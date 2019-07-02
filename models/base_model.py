9d86b649669b0aba9932ff1041c704d37812274a
Rmolimock <495@holbertonschool.com>
2019-07-02 20:15:58 +0000

    serialize as a list of dicts

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,8 @@
 '''
 from datetime import datetime
 import dateutil.parser
+import json
+import models
 from uuid import uuid4
 
 
@@ -12,29 +14,38 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         '''
         '''
-        if kwargs == {}:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        if kwargs:
+            print('UPDATING FROM DICT---------------------------------------------')
+            for (key, value) in kwargs.items():
+                if key in ('updated_at', 'created_at'):
+                    self.key = dateutil.parser.parse(value)
+                elif key == '__class__':
+                    continue
+                else:
+                    self.key = value
         else:
-            self.id = kwargs['id']
-            self.created_at = dateutil.parser.parse(kwargs['created_at'])
-            self.updated_at = dateutil.parser.parse(kwargs['updated_at'])
-
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+    def save(self):
+        models.storage.save()
     def __str__(self):
         '''
         '''
         return("[BaseModel] ({:s}) {:s}".format(self.id, str(self.__dict__)))
-
     def save(self):
         '''
         '''
-        self.updated_at = datetime.now()
-
+        self.updated_at = datetime.utcnow()
+        list_of_dicts = [obj.to_dict() for (key, obj) in models.storage.all().items()]
+        print(type(list_of_dicts))
+        with open(models.storage.file_path, "w+", encoding="utf-8") as f:
+            f.write(json.dumps(list_of_dicts))
     def to_dict(self):
         '''
         '''
-        mydict = self.__dict__
+        mydict = self.__dict__.copy()
         mydict["__class__"] = "BaseModel"
         mydict["updated_at"] = self.updated_at.isoformat()
         mydict["created_at"] = self.created_at.isoformat()
