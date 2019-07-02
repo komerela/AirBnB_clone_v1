8905e0f88ffb859c86487e501574868f5005cad7
Rmolimock <495@holbertonschool.com>
2019-07-02 20:17:03 +0000

    deserializing with loads instead of load

---
+++ models/engine/file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+'''
+'''
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    '''
+    '''
+    __objects = {}
+    __file_path = 'file.json'
+    @property
+    def file_path(self):
+        return FileStorage.__file_path
+    def all(self):
+        '''
+        '''
+        return FileStorage.__objects
+    def new(self, obj):
+        '''
+        '''
+        FileStorage.__objects['{}.{}'.format(obj.__class__.__name__, obj.id)] = obj
+    def reload(self):
+        pass
+        '''
+        '''
+        try:
+            with open('file.json', "r") as f:
+                list_of_dicts = FileStorage.from_json_string(f.read())
+        except FileNotFoundError:
+            list_of_dicts = []
+        for each in list_of_dicts:
+        # use eval to make this flexible
+            BaseModel(each)
+
+
+
+    def from_json_string(json_string):
+        '''convert json string of obj dicts into list of same'''
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
